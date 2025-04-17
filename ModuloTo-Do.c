f1b1fb6ffe850cb22483c351e5ea070c130c3596
NicoMore02 <nicomore053@gmail.com>
2025-04-17 00:35:32 -0300

    tercer commit

--- ModuloTo-Do.c
+++ ModuloTo-Do.c
@@ -19,7 +19,9 @@ typedef struct Nodo
 
 //Funciones del nodo//
 Nodo * CrearListaVacia();
-Nodo *CrearLista (int id);
+Nodo *CrearLista(int id);
+Nodo *BuscarporID(Nodo *lista, int id);
+Nodo *BuscarporPalabra(Nodo *lista, char *palabra);
 
 //Funciones//
 void insertarLista(Nodo **lista, Nodo *nueva);
@@ -45,21 +47,65 @@ int main(int argc, char const *argv[])
 
         switch (opcion)
         {
-        case 1:
-            Nodo *nueva = CrearLista(tareaID++);
-            insertarLista(&pendientes, nueva);
-            break;
-        case 2:
-            printf("Ingrese el ID de la tarea realizada: ");
-            scanf("%d", &buscarID);
-            moverTarea(&pendientes, &realizadas, buscarID);
-            break;
-        case 3:
-            listarTareas(pendientes, "Pendiente");
-            listarTareas(realizadas, "Realizada");
-            break;
-        default:
-            break;
+            case 1: {
+                Nodo *nueva = CrearLista(tareaID++);
+                insertarLista(&pendientes, nueva);
+                break;
+            }
+            case 2:
+                printf("Ingrese el ID de la tarea realizada: ");
+                scanf("%d", &buscarID);
+                moverTarea(&pendientes, &realizadas, buscarID);
+                break;
+            case 3:
+                listarTareas(pendientes, "Pendiente");
+                listarTareas(realizadas, "Realizada");
+                break;
+            case 4:{ 
+                puts("Ingrese 1 para buscar por ID o 2 para buscar por palabra");
+                int tipo;
+                scanf("%d", &tipo);
+                if (tipo == 1) {
+                    printf("Ingrese el ID de la tarea a buscar: ");
+                    scanf("%d", &buscarID);
+                
+                    Nodo *res = BuscarporID(pendientes, buscarID);
+                    if (!res)
+                    {
+                        res = BuscarporID(realizadas, buscarID);
+                    }
+                
+                    if (res != NULL) {
+                        printf("Tarea encontrada: %s (%d min)\n", res->T.Descripcion, res->T.Duracion);
+                    } else
+                    {
+                        puts("Tarea no encontrada");
+                    }
+                
+                } else if (tipo == 2)
+                {
+                    printf("Ingrese la palabra a buscar: ");
+                    getchar();
+                    fgets(palabra, 100, stdin);
+                    Nodo *res = BuscarporPalabra(pendientes, palabra);
+                    if (!res) {
+                        res = BuscarporPalabra(realizadas, palabra);
+                    }
+                    if (res != NULL) {
+                        printf("Tarea encontrada: %s (%d min)\n", res->T.Descripcion, res->T.Duracion);
+                    } else {
+                        puts("No se encontró ninguna tarea con esa palabra");
+                    }
+                
+                }
+                break;
+            }
+            case 5:
+                puts("Saliendo del programa");
+                break;
+            default:
+                puts("Opción no válida");
+                break;
         }
 
     } while (opcion != 5);
@@ -88,7 +134,7 @@ void liberarlista(Nodo *lista) {
 }
 
 void moverTarea(Nodo **pendientes, Nodo **realizadas, int id){
-    Nodo *aux = *pendientes, *anterior = CrearListaVacia;
+    Nodo *aux = *pendientes, *anterior = NULL;
     while (aux && aux->T.TareaID != id){
         anterior = aux;
         aux = aux->siguiente;
@@ -96,6 +142,7 @@ void moverTarea(Nodo **pendientes, Nodo **realizadas, int id){
     if (!aux)
     {
         printf("Tarea no encontrada en pendientes\n");
+        return;
     }
 
     if (anterior)
@@ -114,7 +161,8 @@ void listarTareas(Nodo *lista, char *tipo){
     printf("Tarea %s:\n", tipo);
     while (lista)
     {
-        printf("Tarea: %s || duracion: %d || ID: %d", lista->T.Descripcion, lista->T.Duracion, lista->T.TareaID);
+        printf("Tarea: %s || duracion: %d || ID: %d\n", lista->T.Descripcion, lista->T.Duracion, lista->T.TareaID);
+        lista = lista->siguiente;
     }
     
 }
@@ -129,13 +177,33 @@ Nodo *CrearLista (int id){
     NNodo->T.TareaID = id;
     NNodo->T.Descripcion = (char *)malloc(100 * sizeof(char));
     printf("Descripcion: \n");
-    gets(NNodo->T.Descripcion);
+    getchar();
+    fgets(NNodo->T.Descripcion, 100, stdin);
     do
     {
         printf("Duracion (10 - 100): ");
         scanf("%d", &NNodo->T.Duracion);
         getchar();
     } while (NNodo->T.Duracion < 10 || NNodo->T.Duracion > 100);
-    NNodo->siguiente = CrearListaVacia;
+    NNodo->siguiente = NULL;
     return NNodo;
+}
+
+
+Nodo *BuscarporID(Nodo *lista, int id) {
+    while (lista) {
+        if (lista->T.TareaID == id)
+            return lista;
+        lista = lista->siguiente;
+    }
+    return NULL;
+}
+
+Nodo *BuscarporPalabra(Nodo *lista, char *palabra) {
+    while (lista) {
+        if (strstr(lista->T.Descripcion, palabra))
+            return lista;
+        lista = lista->siguiente;
+    }
+    return NULL;
 }
